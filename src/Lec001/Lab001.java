3a8a5953a580e1fd4264e6986941d49bd2b175fb
ptyagi-123 <priyatyagi119@gmail.com>
2025-01-05 11:52:17 +0530

    Commit

--- src/Lec001/Lab001.java
+++ src/Lec001/Lab001.java
@@ -4,5 +4,7 @@ public class Lab001 {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Priya");
+        System.out.print("Welcome to learning");
+
     }
 }

29714bd88cf60a4bcf6308c38e10ba307a310142
ptyagi-123 <priyatyagi119@gmail.com>
2025-01-05 12:06:06 +0530

    Revert "Commit"
    This reverts commit 3a8a5953a580e1fd4264e6986941d49bd2b175fb.

--- src/Lec001/Lab001.java
+++ src/Lec001/Lab001.java
@@ -4,7 +4,5 @@ public class Lab001 {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Priya");
-        System.out.print("Welcome to learning");
-
     }
 }

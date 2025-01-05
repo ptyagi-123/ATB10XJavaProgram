96c0ab54473fb94ee7a684dd39095f32777823e6
ptyagi-123 <priyatyagi119@gmail.com>
2025-01-05 12:07:50 +0530

    Commit the code

---
+++ src/Prcatice/pra001.java
@@ -0,0 +1,11 @@
+package Prcatice;
+
+public class pra001 {
+    public static void main(String[] args) {
+        int age = 30;
+        String Name ="priya";
+        System.out.println("My name is "+ Name);
+    }
+
+
+}
---
+++ src/Prcatice/pra002.java
@@ -0,0 +1,19 @@
+package Prcatice;
+
+public class pra002 {
+    public static void main(String[] args) {
+        boolean is_employed = true;
+        byte number = 10;
+        short marks = 89;
+        int age = 30;
+        long salary = 70000;
+        char grade = 'A';
+        System.out.println(is_employed);
+        System.out.println(number);
+        System.out.println(marks);
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+
+    }
+}
